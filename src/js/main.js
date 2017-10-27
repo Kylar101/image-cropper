ff2972ab0f7156e24f0b41250f1a50b8f9170dd0
Ben <ba.lowbridge@gmail.com>
2017-10-27 11:03:30 +1000

    removing semi-colons

--- src/js/main.js
+++ src/js/main.js
@@ -8,26 +8,29 @@ $(function () {
 
     'use strict';
 
+    // ----------------------------------------------------------------------------------------------
+    // ---------------------------------- LOADING CODE ---------------------------------------------
+    // ----------------------------------------------------------------------------------------------
 
     let sizesValue = document.getElementById('sizesValue')
     let brandsValue = document.getElementById('brandsValue')
 
     function pageLoad() {
-        // utils.addSizingOptions();
-        // utils.addBrandingOptions();
+        // utils.addSizingOptions()
+        // utils.addBrandingOptions()
         let sizes = localStorage.getItem("imageCropperSizes")
         let brands = localStorage.getItem("imageCropperBrands")
         sizesValue.value = sizes
         brandsValue.value = brands
         if (!sizes) {
-            $('.sizeInstructions').append('<h2 class="select-message no-sizes">Please enter some sizes</h2>');
+            $('.sizeInstructions').append('<h2 class="select-message no-sizes">Please enter some sizes</h2>')
         } else {
             $('.no-sizes').remove()
             utils.addLocalSizingOptions(JSON.parse(sizes))
         }
 
         if (!brands) {
-            $('.brandingInstructions').append('<h2 class="select-message no-brands">Please enter some sizes</h2>');
+            $('.brandingInstructions').append('<h2 class="select-message no-brands">Please enter some brands</h2>')
         } else {
             $('.no-brands').remove()
             utils.addLocalBrandingOptions(JSON.parse(brands))
@@ -38,14 +41,14 @@ $(function () {
     // ---------------------------------- BACKBONE CODE ---------------------------------------------
     // ----------------------------------------------------------------------------------------------
 
-    window.onload = pageLoad();
+    window.onload = pageLoad()
 
     let helpModalID = document.getElementById('helpModal')
     let helpModal = new bsn.Modal(helpModalID)
 
     // opens the helper Modal
     $('.helpButton').click(function () {
-        // $('#helpModal').modal();
+        // $('#helpModal').modal()
         helpModal.show()
     })
 
@@ -82,45 +85,45 @@ $(function () {
     })
 
     // first, we reference the .nav component that holds all tabs
-    var myTabs = document.getElementById('helpTabs');
+    var myTabs = document.getElementById('helpTabs')
 
     // let's give the initialization a JavaScript reference for the "target" option
-    var myTabsCollection = myTabs.getElementsByTagName('A');
+    var myTabsCollection = myTabs.getElementsByTagName('A')
 
     // initialize the component for all items in the collection
     for (var i = 0; i < myTabsCollection.length; i++) {
         new bsn.Tab(myTabsCollection[i], // our target
         { // our options
             height: true
-        });
+        })
     }
 
     // dynamic variables
-    var imageSize;
-    var cropperSizeData = [];
-    var cropperSizeOptions = [];
-    var branding = false;
-    var brandingImage = '';
-    var brandingDetails = [];
+    var imageSize
+    var cropperSizeData = []
+    var cropperSizeOptions = []
+    var branding = false
+    var brandingImage = ''
+    var brandingDetails = []
     // var ratioList = ['one','two','three','four','five','six','seven','eight','nine','ten','eleven','twelve'];
-    var ratioList;
+    var ratioList
 
     // checks for new sizes and adds it to html
     $(document).on('change', 'input[name="dynamicSizes"]', function () {
-        console.log('changed');
+        console.log('changed')
 
         // gets identifiers from sizes
         ratioList = utils.getSizeIdentifiers('dynamicSizes')
 
         var checking = [];
-        imageSize = utils.getCheckBoxesSize('dynamicSizes');
+        imageSize = utils.getCheckBoxesSize('dynamicSizes')
 
         // checks if size already on screen
         for (var i = 0; i < imageSize[0].length; i++) {
             if ($('.' + imageSize[0][i]).length < 1) {
-                utils.addCropperHTMLSize(imageSize[0][i], imageSize[1][i], imageSize[2][i]);
+                utils.addCropperHTMLSize(imageSize[0][i], imageSize[1][i], imageSize[2][i])
             } else {
-                console.log(`element number ${imageSize[0][i]} exists`);
+                console.log(`element number ${imageSize[0][i]} exists`)
             }
         }
 
@@ -128,9 +131,9 @@ $(function () {
         for (var x = 0; x < ratioList.length; x++) {
             if (document.getElementById(ratioList[x])) {
                 if (checking.includes(ratioList[x])) {
-                    console.log('already in array');
+                    console.log('already in array')
                 } else {
-                    checking.push(ratioList[x]);
+                    checking.push(ratioList[x])
                 }
             }
         }
@@ -138,31 +141,31 @@ $(function () {
         // removes sizes if no longer selected
         for (var t = 0; t < checking.length; t++) {
             if (!imageSize[0].includes(checking[t])) {
-                $('#' + checking[t]).remove();
+                $('#' + checking[t]).remove()
             }
         }
 
         // removes all remaining sizes if none selected
         if (imageSize[0].length == 0) {
-            $('.dynamicImages').remove();
+            $('.dynamicImages').remove()
         }
 
         // checks if there are any cropper objects. Displays Select message if false
         if ($(".dynamicImages").length < 1) {
-            $('#croppingArea').append('<h2 class="select-message">Please select an image size</h2>');
+            $('#croppingArea').append('<h2 class="select-message">Please select an image size</h2>')
         } else {
-            $('.select-message').remove();
+            $('.select-message').remove()
         }
 
 
-    });
+    })
 
     var $image;
     // adds cropper objects onto the html
     $(document).on('change', 'input[name="dynamicSizes"]', function () {
         $image = imageSize[3][0];
         for (var i = 0; i < imageSize[0].length; i++) {
-            cropperSizeData[i] = $(imageSize[3][i]);
+            cropperSizeData[i] = $(imageSize[3][i])
             cropperSizeOptions[i] = {
                 aspectRatio: imageSize[1][i] / imageSize[2][i],
                 preview: `.img-preview-${imageSize[0][i]}`,
@@ -179,13 +182,13 @@ $(function () {
 
             // keeps the image the same accross all sizes
             if (blobURL) {
-                var new_blobURL = URL.createObjectURL(file);
+                var new_blobURL = URL.createObjectURL(file)
                 cropperSizeData[i].one('built.cropper', function () {
-                }).cropper('reset').cropper('replace', new_blobURL);
+                }).cropper('reset').cropper('replace', new_blobURL)
             }
         }
 
-    });
+    })
 
     // zooms image into the correct dimensions
     $(document).on('change', 'input[name="dynamicSizes"]', function () {
@@ -204,7 +207,7 @@ $(function () {
     })
     // adds branding
     $(document).on('change', 'input[name="dynamicBrands"],input[name="dynamicSizes"]', function () {
-        var brandingDetails = utils.getBrandingOptions('dynamicBrands');
+        var brandingDetails = utils.getBrandingOptions('dynamicBrands')
 
         brandingImage = brandingDetails[0][0];
 
@@ -239,15 +242,15 @@ $(function () {
             $('.branding-preview').remove()
         }
 
-    });
+    })
 
 
 
     // assigns download button on modal to jquery object
-    var $download = $('#download');
+    var $download = $('#download')
 
     // creates the initial cropper object
-    // var $image = $('.one > img');
+    // var $image = $('.one > img')
     // var options = {
     //   aspectRatio: 572 / 150,
     //   preview: '.img-preview-one',
@@ -258,17 +261,17 @@ $(function () {
     //   // cropBoxMovable: false
     // };
     // // initialises first cropper
-    // $image.cropper(options);
+    // $image.cropper(options)
 
     // Download
     if (typeof $download[0].download === 'undefined') {
-        $download.addClass('disabled');
+        $download.addClass('disabled')
     }
 
     // export button click
     $('.docs-buttons').on('click', '[data-method]', function () {
-        var $this = $(this);
-        var data = $this.data();
+        var $this = $(this)
+        var data = $this.data()
         var $target;
         var result;
         var dynamicResult = [];
@@ -277,13 +280,13 @@ $(function () {
 
             // some error checking
             if (typeof data.target !== 'undefined') {
-                $target = $(data.target);
+                $target = $(data.target)
 
                 if (typeof data.option === 'undefined') {
                     try {
-                        data.option = JSON.parse($target.val());
+                        data.option = JSON.parse($target.val())
                     } catch (e) {
-                        console.log(e.message);
+                        console.log(e.message)
                     }
                 }
             }
@@ -291,17 +294,17 @@ $(function () {
             // adds each of the selected sizes to array of canvas
             if (imageSize) {
                 for (var i = 0; i < imageSize[0].length; i++) {
-                    dynamicResult.push(cropperSizeData[i].cropper(data.method, data.option, data.secondOption));
+                    dynamicResult.push(cropperSizeData[i].cropper(data.method, data.option, data.secondOption))
                 }
             } else {
-                result = $image.cropper(data.method, data.option, data.secondOption);
+                result = $image.cropper(data.method, data.option, data.secondOption)
             }
 
             switch (data.method) {
                 // for rotating image - not applicable atm
                 case 'scaleX':
                 case 'scaleY':
-                    $(this).data('option', -data.option);
+                    $(this).data('option', -data.option)
                     break;
 
                 // brings up download modal
@@ -314,17 +317,17 @@ $(function () {
 
                     // adds images to modal and downloads
                     if (imageSize) {
-                        $('#getCroppedCanvasModal').find('.modal-body').html(dynamicResult);
+                        $('#getCroppedCanvasModal').find('.modal-body').html(dynamicResult)
                         // downloadModal.show()
                     } else {
-                        $('#getCroppedCanvasModal').find('.modal-body').html(result);
+                        $('#getCroppedCanvasModal').find('.modal-body').html(result)
                         // downloadModal.show()
                     }
 
                     if (branding) {
-                        utils.addBrandingAndDownload(brandingImage);
+                        utils.addBrandingAndDownload(brandingImage)
                     } else {
-                        utils.downloadCanvas(this, 'cropper');
+                        utils.downloadCanvas(this, 'cropper')
                     }
 
                     break;
@@ -333,14 +336,14 @@ $(function () {
             // more error checking
             if ($.isPlainObject(result) && $target) {
                 try {
-                    $target.val(JSON.stringify(result));
+                    $target.val(JSON.stringify(result))
                 } catch (e) {
-                    console.log(e.message);
+                    console.log(e.message)
                 }
             }
 
         }
-    });
+    })
 
 
     // ----------------------------------------------------------------------------------------------
@@ -349,7 +352,7 @@ $(function () {
 
 
     // Import image
-    var $inputImage = $('#inputImage');
+    var $inputImage = $('#inputImage')
     var URL = window.URL || window.webkitURL;
     var blobURL;
     var files;
@@ -366,7 +369,7 @@ $(function () {
                 // checks file type
                 if (/^image\/\w+$/.test(file.type)) {
                     // gets uploaded image
-                    blobURL = URL.createObjectURL(file);
+                    blobURL = URL.createObjectURL(file)
 
                     // adds image to all croppers on screen
                     if (imageSize) {
@@ -374,26 +377,26 @@ $(function () {
                             cropperSizeData[i].one('built.cropper', function () {
 
                                 // Revoke when load complete
-                                URL.revokeObjectURL(blobURL);
-                            }).cropper('replace', blobURL);
+                                URL.revokeObjectURL(blobURL)
+                            }).cropper('replace', blobURL)
                         }
                     } else {
                         $image.one('built.cropper', function () {
 
                             // Revoke when load complete
-                            URL.revokeObjectURL(blobURL);
-                        }).cropper('replace', blobURL);
+                            URL.revokeObjectURL(blobURL)
+                        }).cropper('replace', blobURL)
                     }
 
-                    $inputImage.val('');
+                    $inputImage.val('')
                 } else {
                     // error message for wrong file type
-                    swal('Whoops!', 'Please choose an image file', 'error');
+                    swal('Whoops!', 'Please choose an image file', 'error')
                 }
             }
-        });
+        })
     } else {
-        $inputImage.prop('disabled', true).parent().addClass('disabled');
+        $inputImage.prop('disabled', true).parent().addClass('disabled')
     }
 
-});
+})
